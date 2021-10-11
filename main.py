8f10dabbd92e2a93fd3cb5e98fcaa4fc75f42c09
Rami-Ismael <rami.m.ismael@gmail.com>
2021-10-10 21:52:38 -0500

    Add knowledebas code to the main file

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from WebCrawls import *
 from UtilityFuncts import *
 import requests
 
+from kb import KB, Profession
+
 
 # import selenium
 # import lxml
@@ -327,12 +329,72 @@ if __name__ == '__main__':
     throne = "throne"
     tyrion = "tyrion"
     stark = "stark"
-    martin = "martin"
+    robert = "robert"
     arya = "arya"
     cersei = "cersei"
-    dragon  = "dragon"
+    rob  = "rob"
     emilia  = "emilia"
     melisandre = "melisandre"
-    kingdom = "kingdom"
-    top_term = [throne, tyrion, stark, martin , arya , cersei , dragon , emilia , melisandre , kingdom ]
+    jon = "jon"
+    top_term = [throne, tyrion, stark, robert , arya , cersei , rob , emilia , melisandre , jon ]
 
+    ## add throne 
+    kb = KB(Entities= dict())
+
+    ## add throne and
+    freq = loaded_dictionary["throne"]
+    val = qid("throne")
+    relationship = {qid("emilia"):" She wants the throne"}
+    kb.add_entity( name = "throne" , freq = freq , qid = val , relationship = relationship )
+
+
+
+    ## add tyrion
+    freq = loaded_dictionary["tyrion"]
+    val = qid("tyrion")
+    relationship = {qid("cersia"):" younger brother of Cerseia"}
+    kb.add_entity( name = "tyrion" , freq = freq , qid = val , relationship = relationship )
+
+    ## stark
+    freq = loaded_dictionary["stark"]
+    val = qid("stark")
+    kb.add_entity( name = "stark" , freq = freq , qid = val , relationship = None )
+    kb.add_profession(val , Profession.Knight)
+
+    ## robert
+    freq = loaded_dictionary["stark"]
+    val = qid("stark")
+    relationship = {qid("Lord Steffon Baratheron"):" oldest son and heir of Lord Steffon Baratheon"}
+    kb.add_entity( name = "stark" , freq = freq , qid = val , relationship = relationship )
+    kb.add_profession(val , Profession.Lord)
+
+    ## arya 
+    freq = loaded_dictionary["arya"]
+    val = qid("arya")
+    kb.add_entity( name = "arya" , freq = freq , qid = val , relationship = None )
+
+    ## cersei
+    freq = loaded_dictionary["cersei"]
+    val = qid("cersei")
+    kb.add_entity( name = "cersei" , freq = freq , qid = val , relationship = None )
+
+    for x in range( 5, len(top_term)):
+        freq = loaded_dictionary[top_term[x]]
+        val = qid(top_term[x])
+        kb.add_entity( name = top_term[x] , freq = freq , qid = val , relationship = None )
+
+    ## add rob
+    kb.add_realtionship( qid("rob") , {qid("Lord Eddard Stark of Winterfell") : "The eldest sone of Eddard Stark of Winterfell"})
+
+    ## emilia
+    kb.add_realtionship(qid("emilia"), {qid("Targaryen"):"She is a Targaryen"})
+
+    ## melisandre
+    kb.add_realtionship(qid( "melisandre"), {qid("melisandre") : "She is hte Red Priestress"})
+    kb.add_profession(qid("melisandre") , Profession.Priestress) 
+
+    ## jon
+    kb.add_realtionship( qid( "jon") , { qid("lyanna") : "He is the sone of Lyanna Stark " })
+
+
+    kb.save_entities()
